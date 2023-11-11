f15a717b69542e04b7e15bd50aea2ec964bf37cc
sven tajz <sventajz3@gmail.com>
2023-11-11 21:31:24 +0100

    map component now loads the map and marker faster

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -2,22 +2,11 @@
 /* eslint-disable no-undef */
 import { Loader } from "@googlemaps/js-api-loader";
 import axios from "axios";
-import { computed, onMounted, ref } from "vue";
-
+import { computed, onMounted, ref, watch } from "vue";
 //import { geoCode } from '../geocode';
 import { useGeolocation } from "../useGeolocation";
 const GOOGLE_MAPS_API_KEY = process.env.VUE_APP_GOOGLE_API;
 const url = "http://localhost:5000/api/posts";
-
-const data = async function getPosts() {
-  // eslint-disable-next-line no-async-promise-executor
-  const res = await axios.get(url);
-  const data = res.data;
-  console.log(data);
-  return data;
-};
-
-console.log("mapcomp:L ", data);
 
 export default {
   setup() {
@@ -33,11 +22,76 @@ export default {
     onMounted(async () => {
       await loader.load();
 
-      new google.maps.Map(mapDiv.value, {
+      var map = new google.maps.Map(mapDiv.value, {
         center: currPos.value,
         zoom: 2.1,
       });
+      async function run() {
+        const result = await main();
+        console.log("hello", result);
+        return result;
+      }
+      let runVal = await run();
+      var jsVal = JSON.stringify(runVal);
+      jsVal = JSON.parse(jsVal);
+      console.log("jsVal:", jsVal[0]);
+
+      //main();
+
+      for (let i = 0; i < jsVal.length; i++) {
+        var marker = new google.maps.Marker({
+          position: JSON.parse(jsVal[i]),
+          Text: "hello world",
+        });
+        marker.setMap(map);
+        main();
+      }
     });
+    // this function gets the posts from database
+    async function getPosts() {
+      try {
+        const response = await axios.get(url);
+        let array = [];
+        for (let i = 0; i < Object.keys(response.data).length; i++) {
+          array.push(response.data[i].place);
+        }
+        return array;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    async function main() {
+      let locationdata = await getPosts();
+      console.log("this is location data: ", locationdata);
+      console.log(locationdata.length);
+
+      let geoResult = []; // declare geoResult outside of the loop
+
+      // this should loop all the locations and their coordinates and add that marker to the map
+      for (let i = 0; i < locationdata.length; i++) {
+        // assign the value returned by geoCode() function to geoResult
+        geoResult.push(await geoCode(locationdata[i]));
+
+        console.log("geo res: ", geoResult);
+        console.log(geoResult.locationString);
+
+        // You can now use the `geoResult` variable to access the result of `geoCode()` function outside of the loop.
+      }
+
+      console.log("this is geoResult", geoResult);
+      return geoResult;
+    }
+
+    watch(async () => {
+      await getPosts();
+      await main();
+    });
+
+    // onMounted(async () => {
+    //   // function that returns places data
+
+    // });
 
     return { currPos, mapDiv };
   },
@@ -48,7 +102,7 @@ function geoCode(visited) {
       .get("https://maps.googleapis.com/maps/api/geocode/json", {
         params: {
           address: visited,
-          key: process.env.VUE_APP_GOOGLE_API,
+          key: GOOGLE_MAPS_API_KEY,
         },
       })
       .then(function (response) {
